156c5266248ee89e86434a0c72dd2d561410a30e
gamalabdelnassersalah-commits <gamalabdelnassersalah@gmail.com>
2026-09-09 11:03:29 +0300

    Make runtime routing tests variant-safe

--- android-app/app/src/androidTest/java/com/tager/marketplace/TagerRuntimeInstrumentationTest.java
+++ android-app/app/src/androidTest/java/com/tager/marketplace/TagerRuntimeInstrumentationTest.java
@@ -87,7 +87,7 @@ public class TagerRuntimeInstrumentationTest {
         assertEquals(TagerDownloadsActivity.class.getName(), downloads.getComponent().getClassName());
         assertNotNull(products.getComponent());
         assertEquals(TagerActivity.class.getName(), products.getComponent().getClassName());
-        assertEquals("tager://open/products", products.getDataString());
+        assertEquals(scheme + "://open/products", products.getDataString());
     }
 
     @Test
